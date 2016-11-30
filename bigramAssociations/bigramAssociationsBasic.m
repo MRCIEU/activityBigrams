0ea59b61b56bd279eda5066e342fb2d4b231d96a
Louise Millard <lm0423@newblue1.cm.cluster>
2016-11-30 11:25:56 +0000

    added info and removed old code for bigramAssociationsBasic.m

--- bigramAssociations/bigramAssociationsBasic.m
+++ bigramAssociations/bigramAssociationsBasic.m
@@ -1,21 +1,20 @@
 
+### This code is not used in our final analysis.
+### This is a basic test of association of each bigram with BMI. 
+### It looks at whether a change in the frequency of a bigram is associated with BMI.
+### It doesn't look at the associations when one bigram freq increases, coupled with a
+### decrease in freq of another bigram - this is what we look at in the paper.
+
+
 data = getDataSample();
 
 % here bigrams are consecutive pairs (i.e. unordered adjacent activity states)
-%bigrams = data(:, {'missmiss', 'missS', 'missL', 'missM', 'missV', 'Smiss', 'SS', 'SL', 'SM', 'SV', 'Lmiss', 'LS', 'LL', 'LM', 'LV', 'Mmiss', 'MS', 'ML', 'MM', 'MV', 'Vmiss', 'VS', 'VL', 'VM', 'VV'});
 bigrams = data(:, {'SS', 'SL', 'SM', 'SV', 'LS', 'LL', 'LM', 'LV', 'MS', 'ML', 'MM', 'MV', 'VS', 'VL', 'VM', 'VV'});
-%bigrams = mat2dataset(bigrams);
-%bigrams.Properties.VarNames = {'SS','SL','SM','SV','LL','LM','LV','MM','MV','VV'};
-
-%stateCounts1 = [repmat(data.countSed,1,4) repmat(data.countLow,1,4) repmat(data.countMod,1,4) repmat(data.countVig,1,4)];
-%stateCounts2 = [repmat([data.countSed data.countLow data.countMod data.countVig], 1, 4)];
 
 confounders = data(:,{'ethnicity', 'parity', 'matSmPreg', 'hhsoc', 'mated', 'sex', 'age11'});
-%stateCounts = data(:,{'countMissing', 'countSed', 'countLow', 'countMod', 'countVig'});
 stateCounts = data(:,{'countSed', 'countLow', 'countMod', 'countVig'});
 
 h=figure('units','inches','position',[.1 .1 12 4.8]);
-%h=figure('units','inches','position',[.1 .1 15 10]);
 plot([0 size(bigrams,2)+2], [0 0], '--', 'color', 'black');
 xlim([1 size(bigrams,2)+1]);
 ylim([-9 9]);
@@ -25,7 +24,6 @@ set(gca,'fontsize',20);
 xlabel('Bigram', 'FontSize',20);
 ylabel('Change of BMI', 'FontSize',20);
 
-%colorx = {'[1.0 0.6 0.0]';'[0.5 0.8 0.0]';'[0.0 0.0 0.0]'; '[0.9 0.9 0.0]'; '[1.0 0.0 0.0]'; '[0.0 1.0 1.0]'; '[0.0 0.0 1.0]';'[0.0 1.0 0.0]';'[0.2 0.6 0.1]';'[1.0 0.6 0.0]';'[0.5 0.8 0.0]';'[0.0 0.0 0.0]'; '[0.9 0.9 0.0]'; '[1.0 0.0 0.0]'; '[0.0 1.0 1.0]'; '[0.0 0.0 1.0]';'[0.0 1.0 0.0]';'[0.2 0.6 0.1]';'[1.0 0.5 0.5]'; '[0.5 0.5 0.0]'; '[0.5 0.0 0.5]'; '[1.0 0.0 1.0]'; '[0.2 0.5 0.2]';'[0.8 0.6 1.0]';'[0.2 0.2 0.5]'};
 colorx = {'[1.0 0.6 0.0]';'[0.5 0.8 0.0]';'[0.0 0.0 0.0]'; '[0.1 0.1 0.9]'};
 markersx = {'o';'x';'s';'*'};
 markersizex = [10;10;10;10];
@@ -50,7 +48,6 @@ for i=1:size(bigrams,2)
 		B = B*10; BINT = BINT*10;
 
 		fprintf('%.3f \t %.3f, %.3f \t', B(1), BINT(1,1), BINT(1,2));
-		%fprintf(' %s test %d: b=%.3f [%.3f, %.3f] n=%d \n', char(bigrams.Properties.VarNames(:,i)), test, B(1), BINT(1,1), BINT(1,2), size(data,1));
 		hold on;plot(i+test*0.2, B(1), markersx{test}, 'markersize', markersizex(test), 'color', colorx{test}, 'linewidth', 3); 
 		hold on; plot([i+test*0.2 i+test*0.2], [BINT(1,1) BINT(1,2)], 'color', colorx{test}, 'linewidth', 3);
 
