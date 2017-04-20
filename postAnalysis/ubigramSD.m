d3677968b7718802d84139b4c64ed3a2fc740d58
Louise Millard <lm0423@newblue2.cm.cluster>
2017-04-20 18:13:16 +0100

    re-added ubigram SD code

---
+++ postAnalysis/ubigramSD.m
@@ -0,0 +1,20 @@
+
+addpath('..');
+data = getDataSample();
+
+
+ubigrams = [data.SS data.SL+data.LS data.SM+data.MS data.SV+data.VS data.LL data.LM+data.ML data.LV+data.VL data.MM data.MV+data.VM  data.VV];
+ubigrams = mat2dataset(ubigrams);
+ubigrams.Properties.VarNames = {'SS', 'SL', 'SM', 'SV', 'LL', 'LM', 'LV', 'MM', 'MV', 'VV'};  
+
+
+fileID = fopen('../out/ubigram-sd.csv','w');
+fprintf(fileID, 'ubigram \t std \n');
+
+for i=1:size(ubigrams,2)
+
+	fprintf(fileID, '%s \t %.2f \n', ubigrams.Properties.VarNames{i}, std(double(ubigrams(:,i))));
+
+end
+
+fclose(fileID);
